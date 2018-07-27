d43629a3b666ede5d77e6ad7204a033deb4e802d
AllisonR5 <allicraft05@gmail.com>
2018-07-27 14:37:32 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,19 @@
 # Code your instances here
+class GardenGnome
+  attr_accessor :name, :age, :gluten_allergy, :hat_color
+  attr_reader :personality
+  
+  def initialize(personality, name, age, hat_color = red)
+    @personality = evil 
+    @name = name
+    @age = age 
+    @hat_color = hat_color 
+  end 
+  
+  def gnaw 
+    return "Gnawing on a tree!!!"
+  end 
+  
+end
+
+  
